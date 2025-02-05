36581cdc45dca850b83cac360ce456ff0d735b5f
loueysioua <louey.sioua@insat.ucar.tn>
2025-02-05 13:27:58 +0100

    feat: add custom exception classes for better error handling and update AuthController and AdminController to use try-catch blocks for improved error responses

--- api/Presentation/Controllers/UserController.cs
+++ api/Presentation/Controllers/UserController.cs
@@ -23,70 +23,136 @@ namespace api.Presentation.Controllers
         [HttpPost]
         public async Task<ActionResult<ReturnedUserDto>> CreateUser([FromForm] CreateUserDto createUserDto)
         {
-            var createdUser = await _userService.CreateUserAsync(createUserDto);
-            return CreatedAtAction(nameof(GetUserById), new { id = createdUser.Id }, createdUser);
+            try
+            {
+                var createdUser = await _userService.CreateUserAsync(createUserDto);
+                return CreatedAtAction(nameof(GetUserById), new { id = createdUser.Id }, createdUser);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = ex.Message });
+            }
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ReturnedUserDto>>> GetAllUsers()
         {
-            var users = await _userService.GetAllUsersAsync();
-            return Ok(users);
+            try
+            {
+                var users = await _userService.GetAllUsersAsync();
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = ex.Message });
+            }
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<ReturnedUserDto>> GetUserById(Guid id)
         {
-            var user = await _userService.GetUserByIdAsync(id);
-            if (user == null)
+            try
             {
-                return NotFound();
+
+                var user = await _userService.GetUserByIdAsync(id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                return Ok(user);
             }
-            return Ok(user);
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = ex.Message });
+            }
         }
 
         [HttpGet("email/{email}")]
         public async Task<ActionResult<IEnumerable<ReturnedUserDto>>> GetUsersByEmail(string email)
         {
-            var users = await _userService.GetUsersByEmailAsync(email);
-            return Ok(users);
+            try
+            {
+                var users = await _userService.GetUsersByEmailAsync(email);
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = ex.Message
+                });
+            }
         }
 
         [HttpGet("role/{role}")]
         public async Task<ActionResult<IEnumerable<ReturnedUserDto>>> GetUsersByRole(string role)
         {
-            var users = await _userService.GetUsersByRoleAsync(role);
-            return Ok(users);
+            try
+            {
+                var users = await _userService.GetUsersByRoleAsync(role);
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = ex.Message
+                });
+            }
         }
 
         [HttpGet("search/{search}")]
         public async Task<ActionResult<IEnumerable<ReturnedUserDto>>> SearchUsers(string search)
         {
-            var users = await _userService.SearchUsersAsync(search);
-            return Ok(users);
+            try
+            {
+                var users = await _userService.SearchUsersAsync(search);
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = ex.Message
+                });
+            }
         }
 
         [HttpGet("username/{userName}")]
         public async Task<ActionResult<ReturnedUserDto>> GetUserByUserName(string userName)
         {
-            var user = await _userService.GetUserByUserNameAsync(userName);
-            if (user == null)
+            try
             {
-                return NotFound();
+                var user = await _userService.GetUserByUserNameAsync(userName);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                return Ok(user);
             }
-            return Ok(user);
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = ex.Message });
+            }
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult<ReturnedUserDto>> UpdateUser(Guid id, [FromForm] UpdateUserDto updateUserDto)
         {
-            var result = await _userService.UpdateUserAsync(id, updateUserDto);
-            if (!result.Succeeded)
+            try
             {
-                return BadRequest(result.Errors);
+                var result = await _userService.UpdateUserAsync(id, updateUserDto);
+                if (!result.Succeeded)
+                {
+                    return BadRequest(result.Errors);
+                }
+                var updatedUser = await _userService.GetUserByIdAsync(id);
+                return Ok(updatedUser);
             }
-            var updatedUser = await _userService.GetUserByIdAsync(id);
-            return Ok(updatedUser);
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = ex.Message });
+            }
         }
 
         [HttpDelete("{id}")]
@@ -104,30 +170,57 @@ namespace api.Presentation.Controllers
         [Consumes("multipart/form-data")]
         public async Task<ActionResult<string>> UploadProfilePicture(Guid id, UploadProfilePictureDto uploadProfilePictureDto)
         {
-            var profilePicUrl = await _userService.UploadProfilePictureAsync(id, uploadProfilePictureDto);
-            return Ok(profilePicUrl);
+            try
+            {
+                var profilePicUrl = await _userService.UploadProfilePictureAsync(id, uploadProfilePictureDto);
+                return Ok(profilePicUrl);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = ex.Message
+                });
+            }
         }
 
         [HttpPost("{id}/add-role")]
         public async Task<ActionResult> AddRole(Guid id, [FromBody] string role)
         {
-            var result = await _userService.AddRoleAsync(id, role);
-            if (!result.Succeeded)
+            try
             {
-                return BadRequest(result.Errors);
+                var result = await _userService.AddRoleAsync(id, role);
+                if (!result.Succeeded)
+                {
+                    return BadRequest(result.Errors);
+                }
+                return NoContent();
             }
-            return NoContent();
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = ex.Message
+                });
+            }
         }
 
         [HttpGet("verify-email")]
         public async Task<IActionResult> VerifyEmail(Guid userId)
         {
-            var result = await _userService.VerifyEmailAsync(userId);
-            if (!result.Succeeded)
+            try
             {
-                return BadRequest(result.Errors);
+                var result = await _userService.VerifyEmailAsync(userId);
+                if (!result.Succeeded)
+                {
+                    return BadRequest(result.Errors);
+                }
+                return Ok("Email verified successfully.");
             }
-            return Ok("Email verified successfully.");
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = ex.Message });
+            }
         }
     }
 }
